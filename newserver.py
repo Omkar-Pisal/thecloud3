f16ff1193a2dfe750cc4e3a27c36535f97263627
Shivanjay <swagh5@asu.edu>
2023-04-28 19:56:28 -0700

    Latest code

--- newserver.py
+++ newserver.py
@@ -11,24 +11,24 @@ s3 = boto3.client('s3', region_name='us-east-1')
 # Set up Lambda client
 lambda_client = boto3.client('lambda', region_name='us-east-1')
 
-# Set up SQS client
-sqs = boto3.client('sqs', region_name='us-east-1')
+sqs_client = boto3.client('sqs', region_name="us-east-1")
+
+inputSQS_url = ""
 
 # Set up input and output bucket names
-input_bucket = 's3-input-bucket-project3'
-output_bucket = 's3-output-bucket-project3'
+input_bucket = 'cloudy-input-bucket-project2'
+output_bucket = 'cloudy-output-bucket-project2'
 
 # Set up Lambda function name
-function_name = 'your-lambda-function-name'
-
-# Set up SQS queue name
-queue_name = 'academic-info-queue'
-
-# Create the SQS queue if it doesn't exist
-queue = sqs.create_queue(QueueName=queue_name)
+function_name = 'project_2'
 
 # Continuously monitor input bucket for new videos
 while True:
+    messages = sqs_client.receive_message(QueueUrl=inputSQS_url, MaxNumberOfMessages=10,
+                                          WaitTimeSeconds=10, MessageAttributeNames=['All'])
+
+    print("messages:")
+    print(messages)
     response = s3.list_objects_v2(Bucket=input_bucket)
 
     if 'Contents' in response:
@@ -53,23 +53,23 @@ while True:
             academic_info_object = s3.get_object(Bucket=output_bucket, Key=academic_info_key)
             academic_info = academic_info_object['Body'].read().decode('utf-8')
 
-            # Send the academic information to the SQS queue
-            response = sqs.send_message(QueueUrl=queue['QueueUrl'], MessageBody=academic_info)
+            # # Send the academic information to the SQS queue
+            # responsesqs = sqs.send_message(QueueUrl=queue['QueueUrl'], MessageBody=academic_info)
 
             # Delete the academic information object from output bucket
             # s3.delete_object(Bucket=output_bucket, Key=academic_info_key)
 
     # Check the SQS queue for academic information
-    messages = sqs.receive_message(QueueUrl=queue['QueueUrl'], MaxNumberOfMessages=1, WaitTimeSeconds=5)
+    # messages = sqs.receive_message(QueueUrl=queue['QueueUrl'], MaxNumberOfMessages=1, WaitTimeSeconds=5)
 
-    if 'Messages' in messages:
-        for message in messages['Messages']:
-            # Print the academic information on CLI or save it in a file
-            academic_info = message['Body']
-            print(academic_info)
-
-            # Delete the message from the SQS queue
-            sqs.delete_message(QueueUrl=queue['QueueUrl'], ReceiptHandle=message['ReceiptHandle'])
+    # if 'Messages' in messages:
+    #     for message in messages['Messages']:
+    #         # Print the academic information on CLI or save it in a file
+    #         academic_info = message['Body']
+    #         print(academic_info)
+    #
+    #         # Delete the message from the SQS queue
+    #         sqs.delete_message(QueueUrl=queue['QueueUrl'], ReceiptHandle=message['ReceiptHandle'])
 
     # Sleep for 5 seconds before checking for new academic information again
     time.sleep(5)
